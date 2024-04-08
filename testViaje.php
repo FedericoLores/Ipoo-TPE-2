31cc3e3e07d29f67f474130873695940ad0a23ac
dvc0de <federico.lores@est.fi.uncoma.edu.ar>
2024-04-08 18:58:25 -0300

    menu

--- testViaje.php
+++ testViaje.php
@@ -1,4 +1,29 @@
 <?php
 include 'Viaje.php';
 
+//menu
+while ($continuar){
+    echo "seleccione una opción: ";
+    echo "opciones";
+    $opcion = trim(fgets(STDIN));
+    switch($opcion){
+        case 1:
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            break;
+        
+        default:
+            echo "ingrese un número valido";
+    }
+
+}
+
+$continuar = true;
+$opcion;
+
+
 ?>
